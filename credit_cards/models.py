8a4da03345b539b3466772a55d4436d9d8a149ed
Wellisson Nunes <wellissonln@gmail.com>
2023-03-08 14:13:35 -0300

    feat: include 'brand' field in CreditCard model

--- credit_cards/models.py
+++ credit_cards/models.py
@@ -5,4 +5,5 @@ class CreditCard(models.Model):
     exp_date = models.DateField("Expiration Date")
     holder = models.CharField("Card Holder", max_length=50)
     number = models.CharField("Card Number", max_length=100)
-    cvv = models.IntegerField("CVV", max_length=4)
+    cvv = models.IntegerField("CVV")
+    brand = models.CharField("Card Brand", max_length=12)
