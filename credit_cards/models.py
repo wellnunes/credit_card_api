907a51366024c6ece424b2dee1eb5ac5c6b0d631
Wellisson Nunes <wellissonln@gmail.com>
2023-03-08 14:05:08 -0300

    feat: CreditCard initial model created

--- credit_cards/models.py
+++ credit_cards/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class CreditCard(models.Model):
+    exp_date = models.DateField("Expiration Date")
+    holder = models.CharField("Card Holder", max_length=50)
+    number = models.CharField("Card Number", max_length=100)
+    cvv = models.IntegerField("CVV", max_length=4)
